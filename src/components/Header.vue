4d8d828eaff450333c020cde45c3d287d5a9e349
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-20 17:42:57 +0100

    milestone 1

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+      <input type="text" placeholder="Search movie" v-model="inputSearch">
+      <button class="btn" @click="search">Search</button>
+      <ul v-for="movie in movies" :key="movie.id">
+        <li>Titolo : <strong>{{movie.title}}</strong></li>
+        <li>Titolo originale : <strong>{{movie.original_title}}</strong></li>
+        <li>Lingua : <strong>{{movie.original_language}}</strong></li>
+        <li>Voto : <strong>{{movie.vote_average}}</strong></li>
+      </ul>
+  </header>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Header',
+
+  data() {
+    return {
+      inputSearch : '',
+      movies : []
+    }
+  },
+
+  methods : {
+    search() {
+      axios.get('https://api.themoviedb.org/3/search/movie' , {
+          params : {
+            api_key : '53d59c9e8ce0cd47e3b9162e1493c788',
+            query : this.inputSearch
+          }
+      })
+      .then( resp => {
+        this.movies = resp.data.results;
+        console.log(this.movies.results);
+      } )
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
