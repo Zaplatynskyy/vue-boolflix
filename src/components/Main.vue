9b25a21f52fc65593ff9ab4a4732ee0adc7e5849
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-21 17:10:12 +0100

    milestone 4

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,40 +1,46 @@
 <template>
   <main>
-    <div class="row">
-        <div class="box">
-            
+    <div class="content_row">
+        <div class="type">Film</div>
+        <div class="row">
+            <div class="box" v-for="movie in dataShared.movies" :key="movie.id">
+                <img :src="editImg(movie.poster_path)" :alt="movie.original_title">
+
+                <div class="info_hover">
+                    <ul class="info_list">
+                        <li><strong>Titolo</strong> : {{movie.title}}</li>
+                        <li><strong>Lingua</strong> : {{languageFlag(movie.original_language)}}</li>
+                        <li>
+                            <strong>Voto</strong> : 
+                            <i v-show="editVoteStar(movie.vote_average) > 0" v-for="(star) in editVoteStar(movie.vote_average)" :key="star.id" class="fas fa-star"></i>
+                            <i v-show="editVoteStar(movie.vote_average) < 5" v-for="(star) in (5 - editVoteStar(movie.vote_average))" :key="star.id" class="far fa-star"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- lista film -->
-    <!-- <h2>Film</h2>
-    <div class="row movies">
-        <ul v-for="movie in dataShared.movies" :key="movie.id">
-            <li><img :src="editImg(movie.poster_path)" :alt="movie.original_title"></li>
-            <li>Titolo : <strong>{{movie.title}}</strong></li>
-            <li>Titolo originale : <strong>{{movie.original_title}}</strong></li>
-            <li>Lingua : {{languageFlag(movie.original_language)}}</li>
-            <li>
-            Voto : 
-            <i v-show="editVoteStar(movie.vote_average) > 0" v-for="(star) in editVoteStar(movie.vote_average)" :key="star.id" class="fas fa-star"></i>
-            <i v-show="editVoteStar(movie.vote_average) < 5" v-for="(star) in (5 - editVoteStar(movie.vote_average))" :key="star.id" class="far fa-star"></i>
-            </li>
-        </ul>
-    </div> -->
-    <!-- lista serie tv -->
-    <!-- <h2>Serie</h2>
-    <div class="row tv_shows">
-        <ul v-for="tvShow in dataShared.tvShows" :key="tvShow.id">
-        <li><img :src="editImg(tvShow.poster_path)" :alt="tvShow.original_title"></li>
-        <li>Titolo : <strong>{{tvShow.name}}</strong></li>
-        <li>Titolo originale : <strong>{{tvShow.original_name}}</strong></li>
-        <li>Lingua : {{languageFlag(tvShow.original_language)}}</li>
-        <li>
-            Voto : 
-            <i v-show="editVoteStar(tvShow.vote_average) > 0" v-for="(star) in editVoteStar(tvShow.vote_average)" :key="star.id" class="fas fa-star"></i>
-            <i v-show="editVoteStar(tvShow.vote_average) < 5" v-for="(star) in (5 - editVoteStar(tvShow.vote_average))" :key="star.id" class="far fa-star"></i>
-        </li>
-        </ul>
-    </div> -->
+
+    <div class="content_row">
+        <div class="type">Serie</div>
+        <div class="row">
+            <div class="box" v-for="tvShow in dataShared.tvShows" :key="tvShow.id">
+                <img :src="editImg(tvShow.poster_path)" :alt="tvShow.original_title">
+
+                <div class="info_hover">
+                    <ul class="info_list">
+                        <li><strong>Titolo</strong> : {{tvShow.name}}</li>
+                        <li><strong>Lingua</strong> : {{languageFlag(tvShow.original_language)}}</li>
+                        <li>
+                            <strong>Voto</strong> : 
+                            <i v-show="editVoteStar(tvShow.vote_average) > 0" v-for="(star) in editVoteStar(tvShow.vote_average)" :key="star.id" class="fas fa-star"></i>
+                            <i v-show="editVoteStar(tvShow.vote_average) < 5" v-for="(star) in (5 - editVoteStar(tvShow.vote_average))" :key="star.id" class="far fa-star"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
   </main>
 </template>
 
@@ -100,24 +106,64 @@ export default {
 <style lang="scss" scoped>
 
     main {
-        height: calc(100vh - 80px);
+        min-height: calc(100vh - 80px);
         background: rgb(0,0,0);
         background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 84%, rgba(19,19,25,1) 100%); 
         padding: 30px 5px;
-    }
 
-    .row {
-        // background-color: lightgrey;
-        // border: 2px solid black;
-        display: flex;
-        flex-wrap: wrap;
+        .type {
+            color: white;
+            margin: 0 5px;
+        }
 
-        .box {
-            width: calc(100% / 10);
-            height: 300px;
-            background-color: blue;
-            margin: 5px;
+        .row {
+            // background-color: lightgrey;
+            // border: 2px solid black;
+            display: flex;
+            flex-wrap: wrap;
+
+            .box {
+                width: calc(100% / 10);
+                height: 300px;
+                margin: 5px;
+                position: relative;
+                cursor: pointer;
+
+                &:hover .info_hover {
+                    display: block;
+                }
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    object-position: center;
+                }
+
+                .info_hover {
+                    background-color: rgba(0,0,0,.8);
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    display: none;
+
+                    .info_list {
+                        color: white;
+                        list-style: none;
+                        padding: 20px 5px;
+
+                        li {
+                            padding: 5px 0;
+
+                            i {
+                                color: #ffbd00;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-
 </style>
