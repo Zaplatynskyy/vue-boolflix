c3988f48324927767f100f6c793cdffabf72849c
Zaplatynskyy <zaplatynskyy@gmail.com>
2021-12-21 13:16:29 +0100

    edit style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,134 +1,46 @@
 <template>
-  <header>
-      <input type="text" placeholder="Search movie" v-model="inputSearch">
-      <button class="btn" @click="search">Search</button>
-      <!-- lista film -->
-      <h2>Film</h2>
-      <div class="row movies">
-        <ul v-for="movie in movies" :key="movie.id">
-          <li><img :src="editImg(movie.poster_path)" :alt="movie.original_title"></li>
-          <li>Titolo : <strong>{{movie.title}}</strong></li>
-          <li>Titolo originale : <strong>{{movie.original_title}}</strong></li>
-          <li>Lingua : {{languageFlag(movie.original_language)}}</li>
-          <li>
-            Voto : 
-            <i v-show="editVoteStar(movie.vote_average) > 0" v-for="(star) in editVoteStar(movie.vote_average)" :key="star.id" class="fas fa-star"></i>
-            <i v-show="editVoteStar(movie.vote_average) < 5" v-for="(star) in (5 - editVoteStar(movie.vote_average))" :key="star.id" class="far fa-star"></i>
-          </li>
-        </ul>
-      </div>
-      <!-- lista serie tv -->
-      <h2>Serie</h2>
-      <div class="row tv_shows">
-        <ul v-for="tvShow in tvShows" :key="tvShow.id">
-          <li><img :src="editImg(tvShow.poster_path)" :alt="tvShow.original_title"></li>
-          <li>Titolo : <strong>{{tvShow.name}}</strong></li>
-          <li>Titolo originale : <strong>{{tvShow.original_name}}</strong></li>
-          <li>Lingua : {{languageFlag(tvShow.original_language)}}</li>
-          <li>
-            Voto : 
-            <i v-show="editVoteStar(tvShow.vote_average) > 0" v-for="(star) in editVoteStar(tvShow.vote_average)" :key="star.id" class="fas fa-star"></i>
-            <i v-show="editVoteStar(tvShow.vote_average) < 5" v-for="(star) in (5 - editVoteStar(tvShow.vote_average))" :key="star.id" class="far fa-star"></i>
-          </li>
-        </ul>
-      </div>
+    <header>
+        <div class="header_left">
+            <i class="fas fa-bars"></i>
+            <img src="../assets/img/logo.png" alt="">
+        </div>
 
-  </header>
+        <div class="header_right">
+          <i class="fas fa-search"></i>
+          <i class="fas fa-ellipsis-h"></i>
+        </div>
+    </header>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   name: 'Header',
-
-  data() {
-    return {
-      inputSearch : '',
-      movies : [],
-      tvShows : []
-    }
-  },
-
-  methods : {
-    search() {
-      axios.get('https://api.themoviedb.org/3/search/movie' , {
-        params : {
-          api_key : '53d59c9e8ce0cd47e3b9162e1493c788',
-          query : this.inputSearch
-        }
-      })
-      .then( resp => {
-        this.movies = resp.data.results;
-        console.log(this.movies.results);
-      } ),
-
-      axios.get('https://api.themoviedb.org/3/search/tv' , {
-        params : {
-            api_key : '53d59c9e8ce0cd47e3b9162e1493c788',
-            query : this.inputSearch
-          }
-      })
-      .then ( resp => {
-        console.log(resp.data.results);
-        this.tvShows = resp.data.results
-      } )
-
-    },
-
-    languageFlag(lang) {
-      switch(lang) {
-        case 'it':
-          return '🇮🇹'
-
-        case  'fr' : 
-          return '🇫🇷'
-
-        case 'en' :
-          return '🇬🇧'
-
-        case 'de' :
-          return '🇩🇪'
-
-        case 'ru' :
-          return '🇷🇺'
-
-        case 'ja' : 
-          return '🇯🇵'
-
-        case 'zh' :
-          return '🇨🇳'
-
-        default :
-          return lang
-      }
-    },
-
-    editImg(filePath) {
-      const baseUrl = 'https://image.tmdb.org/t/p';
-      const size = '/w154';
-      if(filePath != null) {
-        return baseUrl + size + filePath
-      } else {
-        return 'http://tuscaniagres.it/wp-content/plugins/lightbox/images/No-image-found.jpg'
-      }
-    },
-
-    editVoteStar(vote) {
-      return vote = Math.ceil(vote/2);
-    }
-
-  }
-
 }
 </script>
 
 <style scoped lang="scss">
-  .row {
-    background-color: lightgrey;
-    border: 2px solid black;
-    display: flex;
-    flex-wrap: wrap;
+@import '../assets/style/variables.scss';
 
+  header {
+    height: 80px;
+    background-color : $primary_color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    i {
+        font-size: 22px;
+        color: white;
+        margin: 0 15px;
+    }
+
+    .header_left {
+      display: flex;
+      align-items: center;
+
+      img {
+        height: 40px;
+      }
+    }
   }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,132 @@
+<template>
+  <main>
+    <input type="text" placeholder="Search movie" v-model="inputSearch">
+    <button class="btn" @click="search">Search</button>
+    <!-- lista film -->
+    <h2>Film</h2>
+    <div class="row movies">
+        <ul v-for="movie in movies" :key="movie.id">
+            <li><img :src="editImg(movie.poster_path)" :alt="movie.original_title"></li>
+            <li>Titolo : <strong>{{movie.title}}</strong></li>
+            <li>Titolo originale : <strong>{{movie.original_title}}</strong></li>
+            <li>Lingua : {{languageFlag(movie.original_language)}}</li>
+            <li>
+            Voto : 
+            <i v-show="editVoteStar(movie.vote_average) > 0" v-for="(star) in editVoteStar(movie.vote_average)" :key="star.id" class="fas fa-star"></i>
+            <i v-show="editVoteStar(movie.vote_average) < 5" v-for="(star) in (5 - editVoteStar(movie.vote_average))" :key="star.id" class="far fa-star"></i>
+            </li>
+        </ul>
+    </div>
+    <!-- lista serie tv -->
+    <h2>Serie</h2>
+    <div class="row tv_shows">
+        <ul v-for="tvShow in tvShows" :key="tvShow.id">
+        <li><img :src="editImg(tvShow.poster_path)" :alt="tvShow.original_title"></li>
+        <li>Titolo : <strong>{{tvShow.name}}</strong></li>
+        <li>Titolo originale : <strong>{{tvShow.original_name}}</strong></li>
+        <li>Lingua : {{languageFlag(tvShow.original_language)}}</li>
+        <li>
+            Voto : 
+            <i v-show="editVoteStar(tvShow.vote_average) > 0" v-for="(star) in editVoteStar(tvShow.vote_average)" :key="star.id" class="fas fa-star"></i>
+            <i v-show="editVoteStar(tvShow.vote_average) < 5" v-for="(star) in (5 - editVoteStar(tvShow.vote_average))" :key="star.id" class="far fa-star"></i>
+        </li>
+        </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name : 'Main',
+
+    data() {
+        return {
+            inputSearch : '',
+            movies : [],
+            tvShows : []
+        }
+    },
+
+    methods : {
+        search() {
+            axios.get('https://api.themoviedb.org/3/search/movie' , {
+                params : {
+                api_key : '53d59c9e8ce0cd47e3b9162e1493c788',
+                query : this.inputSearch
+                }
+            })
+            .then( resp => {
+                this.movies = resp.data.results;
+                console.log(this.movies.results);
+            } ),
+
+            axios.get('https://api.themoviedb.org/3/search/tv' , {
+                params : {
+                    api_key : '53d59c9e8ce0cd47e3b9162e1493c788',
+                    query : this.inputSearch
+                }
+            })
+            .then ( resp => {
+                console.log(resp.data.results);
+                this.tvShows = resp.data.results
+            } )
+
+        },
+
+        languageFlag(lang) {
+            switch(lang) {
+                case 'it':
+                return '🇮🇹'
+
+                case  'fr' : 
+                return '🇫🇷'
+
+                case 'en' :
+                return '🇬🇧'
+
+                case 'de' :
+                return '🇩🇪'
+
+                case 'ru' :
+                return '🇷🇺'
+
+                case 'ja' : 
+                return '🇯🇵'
+
+                case 'zh' :
+                return '🇨🇳'
+
+                default :
+                return lang
+            }
+        },
+
+        editImg(filePath) {
+            const baseUrl = 'https://image.tmdb.org/t/p';
+            const size = '/w154';
+            if(filePath != null) {
+                return baseUrl + size + filePath
+            } else {
+                return 'http://tuscaniagres.it/wp-content/plugins/lightbox/images/No-image-found.jpg'
+            }
+        },
+
+        editVoteStar(vote) {
+            return vote = Math.ceil(vote/2);
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        background-color: lightgrey;
+        border: 2px solid black;
+        display: flex;
+        flex-wrap: wrap;
+
+    }
+</style>
